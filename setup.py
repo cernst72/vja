2eb762279639fad23d651de36225958a2cf6928c
Christoph Ernst <christoph.ernst72@googlemail.com>
2023-01-31 13:49:19 +0000

    fix pylint job

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["click", "click-aliases", "requests", "parsedatetime", "python-d
 
 setup(
     name="vja",
-    version="0.0.11",
+    version="0.0.12",
     author="ce72",
     description="A simple CLI for Vikunja task manager",
     long_description=readme,
