7016a49c2ab6ed359666033abf7b100ab2723b96
Christoph Ernst <christoph.ernst72@googlemail.com>
2023-02-01 22:25:07 +0100

    fix dates
    - read dates: strip of timezone, assuming the delivered dates were created in our zone ...
    - write dates: write with our local zone

--- vja/model.py
+++ vja/model.py
@@ -2,8 +2,6 @@ import typing
 from dataclasses import dataclass, field
 from datetime import datetime
 
-from dateutil import tz
-
 
 @dataclass(frozen=True)
 class Namespace:
@@ -90,7 +88,7 @@ class Task:
                    )
 
     def urgency(self):
-        today = datetime.today().astimezone(tz.tzlocal())
+        today = datetime.today()
         if self.due_date:
             duedate = self.due_date
             datediff = (duedate - today).days
@@ -133,7 +131,7 @@ class Task:
 
 def _date_from_json(json_date):
     if json_date and json_date > '0001-01-01T00:00:00Z':
-        return datetime.fromisoformat(json_date)
+        return datetime.fromisoformat(json_date.replace("Z", "")).replace(tzinfo=None)
     return None
 
 
