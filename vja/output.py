2b010877d1582408722f719ab985ce10318f9182
cernst72 <christoph.ernst72@googlemail.com>
2023-06-05 20:50:28 +0200

    feat(ls): output flags for alarm, repeat, description

--- vja/output.py
+++ vja/output.py
@@ -14,8 +14,8 @@ LABEL_LIST_FORMAT_DEFAULT = '{x.id:5} {x.title:20.20}'
 
 TASK_LIST_FORMAT_DEFAULT = '{x.id:5} ({x.priority}) {"*" if x.is_favorite else " "} {x.title:50.50} ' \
                            '{x.due_date.strftime("%a %d.%m %H:%M") if x.due_date else "":15.15} ' \
-                           '{"R" if x.reminders else " "} {x.project.title:20.20} ' \
-                           '{x.labels:20.20} {x.urgency:3.1f}'
+                           '{"A" if x.reminders else " "}{"R" if x.repeat_after else " "}{"D" if x.description else " "} ' \
+                           '{x.project.title:20.20} {x.labels:20.20} {x.urgency:3.1f}'
 
 logger = logging.getLogger(__name__)
 
