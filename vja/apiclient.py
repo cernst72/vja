dbfc6b4617a3303e976f278e1d36bae075300c02
cernst72 <christoph.ernst72@googlemail.com>
2024-06-04 18:21:58 +0200

    feat(ls): apply new filter syntax

--- vja/apiclient.py
+++ vja/apiclient.py
@@ -147,7 +147,7 @@ class ApiClient:
     def get_tasks(self, exclude_completed=True):
         if self._cache['tasks'] is None:
             url = self._create_url('/tasks/all')
-            params = {'filter_by': 'done', 'filter_value': 'false'} if exclude_completed else {}
+            params = {'filter': 'done=false'} if exclude_completed else {}
             self._cache['tasks'] = self._get_json(url, params) or []
         return self._cache['tasks']
 
